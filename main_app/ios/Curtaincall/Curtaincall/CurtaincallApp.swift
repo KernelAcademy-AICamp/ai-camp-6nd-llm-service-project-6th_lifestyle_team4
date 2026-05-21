b15fd71e314c6abe1d4a269860d80fed5db996dd
Yub <yub.hahm@gmail.com>
2026-05-21 17:48:16 +0900

    scaffold ios xcode project

---
+++ main_app/ios/Curtaincall/Curtaincall/CurtaincallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurtaincallApp.swift
+//  Curtaincall
+//
+//  Created by Yub Hahm on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CurtaincallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
